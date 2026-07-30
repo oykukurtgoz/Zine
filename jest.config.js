d4f108b9711f76747e5352a0ecf6a982c21deeee
Öykü Kurtgöz <oykukurtgoz@gmail.com>
2026-07-30 18:08:17 +0300

    Add jest-junit reporter and update .gitignore for test reports

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest.setup.js'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { outputDirectory: 'test-reports', outputName: 'unit-report.xml' },
+    ],
+  ],
 };
